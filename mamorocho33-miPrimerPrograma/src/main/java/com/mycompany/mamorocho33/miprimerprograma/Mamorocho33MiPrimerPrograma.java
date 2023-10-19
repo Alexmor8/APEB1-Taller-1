23aa6cd98a3baaf65a8864d1c1ad22de93af41b4
Mario Morcho <mamorocho33@utpl.edu.ec>
2023-10-19 17:14:45 -0500

    Este es mi primer programa subido

---
+++ mamorocho33-miPrimerPrograma/src/main/java/com/mycompany/mamorocho33/miprimerprograma/Mamorocho33MiPrimerPrograma.java
@@ -0,0 +1,6 @@
+package com.mycompany.mamorocho33.miprimerprograma;
+public class Mamorocho33MiPrimerPrograma {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
